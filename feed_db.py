4d6e4cad63cb334a288dc95293af1b826e29f8e8
keeblers-n-bits <kelly.ebler@gmail.com>
2019-10-24 09:41:17 -0700

    undoing comments

--- feed_db.py
+++ feed_db.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 import bs4, requests
 
-# db_connect = create_engine("sqlite:///flavortown.db")
+db_connect = create_engine("sqlite:///flavortown.db")
 
 def scrape_flavor():
     r = requests.get('http://highwaytotheflavorzone.com')
@@ -11,11 +11,10 @@ def scrape_flavor():
     flavor = bs4.BeautifulSoup(r.text, features='html.parser')
     title = flavor.h1.contents
     description = flavor.h2.contents
-    print(title, description)
+    return title, description
 
-# def write_menu_item(title, description):
-#    conn = db_connect.connect()
-#    query = conn.execute(
-#        f"INSERT INTO menu (title, description) VALUES ({title}, {description});"
-#    )
-scrape_flavor()
+def write_menu_item(title, description):
+    conn = db_connect.connect()
+    query = conn.execute(
+        f"INSERT INTO menu (title, description) VALUES ({title}, {description});"
+    )
