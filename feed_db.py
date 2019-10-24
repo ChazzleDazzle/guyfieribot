94a11ade1fdd3f234599860b168f003636128d1c
keeblers-n-bits <kelly.ebler@gmail.com>
2019-10-24 09:37:17 -0700

    added scraper to feed_db.py

--- feed_db.py
+++ feed_db.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+import bs4, requests
 
-db_connect = create_engine("sqlite:///flavortown.db")
+# db_connect = create_engine("sqlite:///flavortown.db")
 
+def scrape_flavor():
+    r = requests.get('http://highwaytotheflavorzone.com')
+    r.raise_for_status()
+    flavor = bs4.BeautifulSoup(r.text, features='html.parser')
+    title = flavor.h1.contents
+    description = flavor.h2.contents
+    print(title, description)
 
-def write_menu_item(title, description):
-    conn = db_connect.connect()
-    query = conn.execute(
-        f"INSERT INTO menu (title, description) VALUES ({title}, {description});"
-    )
+# def write_menu_item(title, description):
+#    conn = db_connect.connect()
+#    query = conn.execute(
+#        f"INSERT INTO menu (title, description) VALUES ({title}, {description});"
+#    )
+scrape_flavor()
