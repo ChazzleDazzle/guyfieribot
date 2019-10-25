ab5e43692afe642833542a1d66a51d3277766508
keeblers-n-bits <kelly.ebler@gmail.com>
2019-10-25 14:38:49 -0700

    feed_db.py adds 500 rows to flavortown.db

--- feed_db.py
+++ feed_db.py
@@ -9,12 +9,23 @@ def scrape_flavor():
     r = requests.get('http://highwaytotheflavorzone.com')
     r.raise_for_status()
     flavor = bs4.BeautifulSoup(r.text, features='html.parser')
-    title = flavor.h1.contents
-    description = flavor.h2.contents
-    return title, description
+    t = flavor.h1.contents
+    d = flavor.h2.contents
+    return t, d
 
 def write_menu_item(title, description):
     conn = db_connect.connect()
     query = conn.execute(
-        f"INSERT INTO menu (title, description) VALUES ({title}, {description});"
+        f'INSERT INTO menu (title, description) VALUES ("{title}", "{description}");'
     )
+
+# title = scrape_flavor()[0]
+# description = scrape_flavor()[1]
+
+for i in range (0, 500):
+    title = scrape_flavor()[0][0]
+    description = scrape_flavor()[1][0]
+    print(title, description)
+    write_menu_item(title, description)
+    i = i + 1
+    
